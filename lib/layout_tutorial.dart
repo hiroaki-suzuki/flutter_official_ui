87ef768ed2c8af990461c7c92fde643d0daf8c43
Hiroaki Suzuki <rirazou05410@gmail.com>
2020-04-27 22:43:08 +0900

    Step 0: Create the app base code

---
+++ lib/layout_tutorial.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter layout demo'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
